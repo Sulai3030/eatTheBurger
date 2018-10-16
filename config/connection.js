0fdda5d6a38613ff00bd9b27c534f8728c01fa5b
sully34 <sssulai@gmail.com>
2018-10-15 22:19:45 -0400

    updated connection.js in config folder

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,32 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+// var connection = mysql.createConnection({
 
-  if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+//   if (process.env.JAWSDB_URL){
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host:'yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//     user:'x6uiiynrdjwyd87h',
+//     password:'vopt415vuxknd7if',
+//     database:'szf6czp8fqw41tw2'
+//   });
+// };
+
+var connection = null;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
+  //here rather than using heroku credentials, you want to use your local
+    connection = mysql.createConnection({
     host:'yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user:'x6uiiynrdjwyd87h',
     password:'vopt415vuxknd7if',
     database:'szf6czp8fqw41tw2'
   });
-};
+
 
 // Make connection.
 connection.connect(function(err) {
